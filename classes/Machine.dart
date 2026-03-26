fd3cc60a5ecdd1e137c75768e55991496f01d4a0
Nikita Grigorev <GrigorevNikOfficial@icloud.com>
2026-03-27 00:11:38 +0300

    Создал класс Machine, добавил геттеры и сеттеры

---
+++ classes/Machine.dart
@@ -0,0 +1,30 @@
+class Machine {
+  
+  int _coffeeBeans;
+  int _milk;
+  int _water;
+  int _cash;
+
+  Machine(this._coffeeBeans, this._milk, this._water, this._cash);
+
+  int get coffeeBeans {return _coffeeBeans;}
+  set coffeeBeans(int value) {
+    _coffeeBeans = value;
+  }
+
+  int get milk {return _milk;}
+  set milk(int value) {
+    _milk = value;
+  }
+
+  int get water {return _water;}
+  set water (int value) {
+    _water = value;
+  }
+
+  int get cash {return _cash;}
+  set cash (int value) {
+    _cash = value;
+  }
+
+}
