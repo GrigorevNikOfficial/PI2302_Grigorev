1b5316b3d127f47cc1afb0e9a14897f99811117f
Nikita Grigorev <GrigorevNikOfficial@icloud.com>
2026-05-11 16:43:45 +0300

    Созданы классы для кофе

---
+++ classes/Coffee.dart
@@ -0,0 +1,49 @@
+import './ICoffee.dart';
+
+class Cappuccino implements ICoffee {
+  const Cappuccino();
+
+  @override
+  int coffeeBeans() => 50;
+
+  @override
+  int milk() => 100;
+
+  @override
+  int water() => 100;
+
+  @override
+  int cash() => 150;
+}
+
+class Espresso implements ICoffee {
+  const Espresso();
+
+  @override
+  int coffeeBeans() => 50;
+
+  @override
+  int milk() => 0;
+
+  @override
+  int water() => 100;
+
+  @override
+  int cash() => 100;
+}
+
+class Americano implements ICoffee {
+  const Americano();
+
+  @override
+  int coffeeBeans() => 50;
+
+  @override
+  int milk() => 0;
+
+  @override
+  int water() => 150;
+
+  @override
+  int cash() => 120;
+}
